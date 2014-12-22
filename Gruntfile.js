32e1699bfcfbe032916e4ea0f01b6d60898c4d4f
Thanh Tran <trongthanh@gmail.com>
2014-12-22 11:16:07 +0700

    Remove modernizr.js; Apply grunt-inline to automate CSS inlining

--- Gruntfile.js
+++ Gruntfile.js
@@ -88,7 +88,6 @@ module.exports = function(grunt) {
 			compile: { // Target
 				options: { // Target options
 					style: 'expanded'
-					// sourcemap: true //need SASS 3.3, install with: gem install sass --pre
 				},
 
 				files: [{
@@ -101,7 +100,8 @@ module.exports = function(grunt) {
 			},
 			dist: { // Target
 				options: { // Target options
-					style: 'compressed'
+					style: 'compressed',
+					sourcemap: 'none'
 				},
 
 				files: [{
@@ -191,6 +191,16 @@ module.exports = function(grunt) {
 			server: '.tmp'
 		},
 
+		inline: {
+			dist: {
+				options:{
+					// uglify: true
+				},
+				src: ['<%= config.dist %>/index.html'],
+				dest: ['<%= config.dist %>/']
+			}
+		},
+
 		cdn: {
 			html: {
 				options: {
@@ -229,6 +239,7 @@ module.exports = function(grunt) {
 		'modernizr',
 		'concat',
 		'uglify',
+		'inline',
 		'filerev',
 		'usemin'
 	]);
